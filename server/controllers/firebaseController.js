daa8b2dfca5a23ee832910e45917ce96a1494dac
Haxous742 <vedantmundada6@gmail.com>
2025-08-12 19:08:16 +0530

    FirebaseController

--- server/controllers/firebaseController.js
+++ server/controllers/firebaseController.js
@@ -9,15 +9,25 @@ router.post("/firebase-login", async (req, res) => {
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
-    const { uid, email } = decodedToken;
+    const { uid, email, name, picture } = decodedToken;
 
     let user = await Users.findOne({ email : email});
 
     if (!user) {
-      user = await Users.create({ email });
+      // Parse name if provided
+      const nameParts = name ? name.split(' ') : [];
+      const firstname = nameParts[0] || '';
+      const lastname = nameParts.slice(1).join(' ') || '';
+      
+      user = await Users.create({ 
+        email,
+        firstname,
+        lastname,
+        profilePic: picture || ''
+      });
     }
 
-    const token = jwt.sign({ uid, email }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
 
@@ -28,7 +38,18 @@ router.post("/firebase-login", async (req, res) => {
       maxAge: 24 * 60 * 60 * 1000,
     });
 
-    res.json({ message: "Login successful" });
+    res.json({ 
+      message: "Login successful",
+      success: true,
+      data: {
+        user: {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email,
+          profilePic: user.profilePic
+        }
+      }
+    });
   } catch (err) {
     res.status(401).json({ message: "Invalid Firebase ID token" });
   }
