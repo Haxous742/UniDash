dae0c0963daae20367d1464be69003705525a1e5
Haxous742 <vedantmundada6@gmail.com>
2025-08-15 14:31:56 +0530

    render config -2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
   
   return {
     plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: env.VITE_API_URL,
+    //       changeOrigin: true,
+    //       secure: false,
+    //     },
+    //   },
+    // },
     css: {
       postcss: './postcss.config.js',
     },
